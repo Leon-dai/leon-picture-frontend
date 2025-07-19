10ddd4dcc17c53f5dba54c8bb53c6c9e832dceea
leon <212649354@qq.com>
2025-07-19 15:23:56 +0800

    用户模块前端实现

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -19,37 +19,41 @@
       </a-col>
       <a-col flex="120px">
         <div class="user-login-status">
-          <a-button type="primary" href="/user/login">登录</a-button>
+          <div v-if="loginUserStore.loginUser.id">
+            <a-dropdown>
+              <ASpace>
+                <a-avatar :src="loginUserStore.loginUser.userAvatar" />
+                {{ loginUserStore.loginUser.userName ?? '无名' }}
+              </ASpace>
+              <template #overlay>
+                <a-menu>
+                  <a-menu-item @click="doLogout">
+                    <LogoutOutlined />
+                    退出登录
+                  </a-menu-item>
+                </a-menu>
+              </template>
+            </a-dropdown>
+          </div>
+
+          <div v-else>
+            <a-button type="primary" href="/user/login">登录</a-button>
+          </div>
         </div>
+
       </a-col>
     </a-row>
   </div>
 </template>
 <script lang="ts" setup>
-import { h, ref } from 'vue'
+import { computed, h, ref } from 'vue'
 import { HomeOutlined } from '@ant-design/icons-vue'
-import { MenuProps } from 'ant-design-vue'
+import { MenuProps, message } from 'ant-design-vue'
 import { useRouter } from 'vue-router'
+import { useLoginUserStore } from '@/stores/useLoginUserStore.ts'
+import { userLogoutUsingPost } from '@/api/userController.ts'
 
 
-const items = ref<MenuProps['items']>([
-  {
-    key: '/',
-    icon: () => h(HomeOutlined),
-    label: '主页',
-    title: '主页',
-  },
-  {
-    key: '/about',
-    label: '关于',
-    title: '关于',
-  },
-  {
-    key: 'others',
-    label: h('a', { href: 'https://github.com/Leon-dai', target: '_blank' }, 'Github主页'),
-    title: '我的Github主页',
-  },
-])
 const router = useRouter()
 // 当前要高亮的菜单项
 const current = ref<string[]>([])
@@ -60,10 +64,62 @@ router.afterEach((to, from, next) => {
 // 路由跳转事件
 const doMenuClick = ({ key }) => {
   router.push({
-    path: key,
+    path: key
   })
 }
+const loginUserStore = useLoginUserStore()
+// 用户注销
+const doLogout = async () => {
+  const res = await userLogoutUsingPost()
+  console.log(res)
+  if (res.data.code === 0) {
+    loginUserStore.setLoginUser({
+      userName: '未登录',
+    })
+    message.success('退出登录成功')
+    await router.push('/user/login')
+  } else {
+    message.error('退出登录失败，' + res.data.message)
+  }
+}
+// 菜单列表
+const originItems = [
+  {
+    key: '/',
+    icon: () => h(HomeOutlined),
+    label: '主页',
+    title: '主页'
+  },
+  {
+    key: '/admin/userManage',
+    label: '用户管理',
+    title: '用户管理'
+  },
+  {
+    key: 'others',
+    label: h('a', { href: 'https://github.com/Leon-dai', target: '_blank' }, 'Github主页'),
+    title: '我的Github主页'
+  }
+]
+
+// 过滤菜单项
+const filterMenus = (menus = [] as MenuProps['items']) => {
+  return menus?.filter((menu) => {
+    if (menu.key.startsWith('/admin')) {
+      const loginUser = loginUserStore.loginUser
+      if (!loginUser || loginUser.userRole !== "admin") {
+        return false
+      }
+    }
+    return true
+  })
+}
+
+// 展示在菜单的路由数组
+const items = computed<MenuProps['items']>(() => filterMenus(originItems))
+
 </script>
+
 
 <style scoped>
 #globalHeader .title-bar {
